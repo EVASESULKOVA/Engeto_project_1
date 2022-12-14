8568731850c9062318ae12e424ab69cb5d2111cf
EVASESULKOVA <116756927+EVASESULKOVA@users.noreply.github.com>
2022-12-14 14:51:36 +0100

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -33,156 +33,83 @@ other freshwater genera and herring similar to those
 in modern oceans. Other fish such as paddlefish,
 garpike and stingray are also present.'''
          ]
-cara = 60 * "-"
-oddelovac = f"""
-{cara}
+line = 60 * "-"
+separator = f"""
+{line}
 """
-slovnik_textu = dict(enumerate(TEXTS, start=1))
-#  print(slovnik_textu.keys())
+dictionary_texts = dict(enumerate(TEXTS, start=1))
+#  print(dictionary_texts.keys())
 
-#  TODO zadejte prihlasovaci udaje
+#  signing of a user
 
-uzivatelske_jmeno = input("Zadejte sve uzivatelske jmeno: ", )
-heslo = input("Zadejte sve uzivatelske heslo: ", )
+user_name = input("Enter your user name: ", )
+password = input("Enter your password: ", )
 
-#  TODO je uzivatel mezi registrovanymi uzivateli? Pokud ano, pozdravit
+#  control of the credentials of the user
 
-registrovani_uzivatele = {
+registered_users = {
     "bob": 123,
     "ann": "pass123",
     "mike": "password123",
     "liz": "pass123"
 }
 
-if uzivatelske_jmeno in registrovani_uzivatele.keys():
-    print(f"Vitej na nasem portale, {uzivatelske_jmeno.capitalize()}\n")
-    #  TODO uživatel si vybere ze 3 textu
-    vybrany_text = input(
-        "Vyberte si jeden z textu k analyze. Pro spravne zvoleni textu, napiste cislici 1, 2, nebo 3: ")
-    #  print(type(vybrany_text))
+if user_name in registered_users.keys():
+    print(f"{separator}Welcome on the portal, {user_name.capitalize()}\n")
 
-    if vybrany_text.isnumeric() and (1 <= int(vybrany_text) <= 3):
-        print(f"""Budeme analyzovat tento text: 
-        '{slovnik_textu[int(vybrany_text)]}'""")
+    #  choose a text for analyses
 
-        #  TODO analyza vybraneho textu - rozdeleni textu na jednotliva slova
-        vycistena_slova = list()
-        jednotliva_slova = slovnik_textu[int(vybrany_text)].split()
+    selected_text = input(
+        "Please, select a text for the analyses. Use one of the following options: 1, 2, or 3: ")
 
-        for slovo in jednotliva_slova:
-            vycistena_slova.append(slovo.strip(",.:;?!-\n"))
-        #  print(vycistena_slova)
+    if selected_text.isnumeric() and (1 <= int(selected_text) <= 3):
+        print(f"""The following text will be analyzed:
+        '{dictionary_texts[int(selected_text)]}'""")
 
-        #  TODO spocitam pocet slov ve vybranem textu
-        print(f"{oddelovac}Vami vybrany text obsahuje {len(vycistena_slova)} slov.")
+        #  division of the text to separated words + analyses of a selected text
+        words_cleaned = list()
+        words_separated = dictionary_texts[int(selected_text)].split()
+        titlecase_words = list()
+        uppercase_words = list()
+        lowercase_words = list()
+        numeric_string = list()
+        sum_of_numbers = 0
+        for word in words_separated:
+            words_cleaned.append(word.strip(",.:;?!-\n"))
+            if word.istitle():
+                titlecase_words.append(word)
+            if word.isalpha() and word.isupper():
+                uppercase_words.append(word)
+            if word.islower():
+                lowercase_words.append(word)
+            if word.isnumeric():
+                numeric_string.append(word)
+            if word.isnumeric():
+                sum_of_numbers += int(word)
+        print(f"{separator}Your chosen text includes {len(words_cleaned)} words.")
+        print(f"Your chosen text includes {len(titlecase_words)} titlecase words.")
+        print(f"Your chosen text includes {len(uppercase_words)} upper case words.")
+        print(f"Your chosen texts includes {len(lowercase_words)} lower case words.")
+        print(f"There are {len(numeric_string)} numeric strings in your chosen text.")
+        print(f"The sum of all the numbers is {sum_of_numbers}.{separator}")
 
-        #  TODO spocitam pocet slov zacinajicich velkym pismenem
-        slova_s_velkym = list()
-        for slovo in vycistena_slova:
-            if slovo.istitle():
-                slova_s_velkym.append(slovo)
-        print(f"Pocet slov zacinajicich velkym pismenem je {len(slova_s_velkym)} slov.")
+        #  analyses of the length of the words in the chosen text
 
-        #  TODO spocitam pocet slov psanych velkymi pismeny
-        slova_velce = list()
-        for slovo in vycistena_slova:
-            if slovo.isalpha() and slovo.isupper():
-                slova_velce.append(slovo)
-        print(f"Pocet slov psanych velkymi pismeny je {len(slova_velce)} slov.")
+        length_of_words = {}
+        for word in words_cleaned:
+            if len(word) not in length_of_words.keys():
+                length_of_words[len(word)] = 1
+            else:
+                length_of_words[len(word)] += 1
 
-        #  TODO spocitam pocet slov psanych malymi pismeny
-        slova_s_velkym = list()
-        for slovo in vycistena_slova:
-            if slovo.islower():
-                slova_s_velkym.append(slovo)
-        print(f"Pocet slov psanych malymi pismeny je {len(slova_s_velkym)} slov.")
+        print(f"{'LENGTH': <10}|{'NUMBER': <10}|{'OCURRENCES': <50} {separator}")
 
-        #  TODO spocitam pocet cisel (ne cifer)
-        slova_jako_cisla = list()
-        for slovo in vycistena_slova:
-            if slovo.isnumeric():
-                slova_jako_cisla.append(slovo)
-        print(f"Pocet cisel v textu je {len(slova_jako_cisla)}.")
+        for key in sorted(length_of_words):
+            print(f"{key: <10}|{length_of_words[key]: <10}|{(length_of_words[key]) * '*': <50}")
 
-        #  TODO spocitam sumu cisel (ne cifer)
-        suma_cisel = 0
-        for slovo in vycistena_slova:
-            if slovo.isnumeric():
-                suma_cisel += int(slovo)
-        print(f"Suma cisel v textu je {suma_cisel}.")
 
-        #  TODO četnost různých délek slov v textu
-        delka_1 = 0
-        delka_2 = 0
-        delka_3 = 0
-        delka_4 = 0
-        delka_5 = 0
-        delka_6 = 0
-        delka_7 = 0
-        delka_8 = 0
-        delka_9 = 0
-        delka_10 = 0
-        delka_11 = 0
-        delka_12 = 0
-        delka_13 = 0
-        delka_14 = 0
-        delka_15 = 0
-        delka_16 = 0
-        for slovo in vycistena_slova:
-            if len(slovo) == 1:
-                delka_1 += 1
-            elif len(slovo) == 2:
-                delka_2 += 1
-            elif len(slovo) == 3:
-                delka_3 += 1
-            elif len(slovo) == 4:
-                delka_4 += 1
-            elif len(slovo) == 5:
-                delka_5 += 1
-            elif len(slovo) == 6:
-                delka_6 += 1
-            elif len(slovo) == 7:
-                delka_7 += 1
-            elif len(slovo) == 8:
-                delka_8 += 1
-            elif len(slovo) == 9:
-                delka_9 += 1
-            elif len(slovo) == 10:
-                delka_10 += 1
-            elif len(slovo) == 11:
-                delka_11 += 1
-            elif len(slovo) == 12:
-                delka_12 += 1
-            elif len(slovo) == 13:
-                delka_13 += 1
-            elif len(slovo) == 14:
-                delka_14 += 1
-            elif len(slovo) == 15:
-                delka_15 += 1
-            elif len(slovo) == 16:
-                delka_16 += 1
-        print(f"""
-        LEN | OCCURENCIES          | NR.
-        1   | {(delka_1 * "*"):<20} | {delka_1}
-        2   | {(delka_2 * "*"):<20} | {delka_2}
-        3   | {(delka_3 * "*"):<20} | {delka_3}
-        4   | {(delka_4 * "*"):<20} | {delka_4}
-        5   | {(delka_5 * "*"):<20} | {delka_5}
-        6   | {(delka_6 * "*"):<20} | {delka_6}
-        7   | {(delka_7 * "*"):<20} | {delka_7}
-        8   | {(delka_8 * "*"):<20} | {delka_8}
-        9   | {(delka_9 * "*"):<20} | {delka_9}
-        10  | {(delka_10 * "*"):<20} | {delka_10}
-        11  | {(delka_11 * "*"):<20} | {delka_11}
-        12  | {(delka_12 * "*"):<20} | {delka_12}
-        13  | {(delka_13 * "*"):<20} | {delka_13}  
-        14  | {(delka_14 * "*"):<20} | {delka_14}
-        15  | {(delka_15 * "*"):<20} | {delka_15}
-        16  | {(delka_16 * "*"):<20} | {delka_16}                
-  
-              """)
     else:
-        print("Pro spravne zvoleni textu, napiste cislici 1, 2, nebo 3. Vami zvolena hodnota neodpovida teto podmince.")
+        print(f"For correct selection of the analysed text, please enter 1, 2 or 3. The entered value {selected_text} doesn't meet this condition")
 
 else:
-    print(f"Uzivatel {uzivatelske_jmeno.capitalize()} neni registrovany. Pred pouzitim portalu se, prosim, registrujte")
+    print(f"The user {user_name.capitalize()} is not registered on the portal. Please, register")
